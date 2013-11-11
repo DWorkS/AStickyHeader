c9149deb1459615f684d933b3db6b519852ee526
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2013-11-11 23:10:47 +0530

    Added content

---
+++ sample/src/dev/dworks/libs/astickyheader/demo/CacheActivity.java
@@ -0,0 +1,194 @@
+package dev.dworks.libs.astickyheader.demo;
+
+import java.lang.ref.WeakReference;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.util.LruCache;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ImageView;
+import dev.dworks.libs.astickyheader.R;
+
+public class CacheActivity extends Activity implements OnScrollListener {
+
+	private Bitmap mPlaceHolderBitmap;
+	private LruCache<String, Bitmap> mMemoryCache;
+    protected boolean mPauseWork = false;
+    private final Object mPauseWorkLock = new Object();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initCache();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setPauseWork(true);
+	}
+	
+    @Override
+    public void onPause() {
+        super.onPause();
+        setPauseWork(false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMemoryCache.evictAll();
+    }
+
+	private void initCache() {
+		mPlaceHolderBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.empty_photo);
+	    final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+	    final int cacheSize = maxMemory / 2;
+
+	    mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+	        @SuppressLint("NewApi") @Override
+	        protected int sizeOf(String key, Bitmap bitmap) {
+	            if (Build.VERSION.SDK_INT >= 12) {
+	                return bitmap.getByteCount()/ 1024;
+	            }
+	            else{
+		            return (bitmap.getRowBytes() * bitmap.getHeight())/ 1024;
+	            }
+	        }
+	    };
+	}
+	
+	public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+	    if (getBitmapFromMemCache(key) == null) {
+	        mMemoryCache.put(key, bitmap);
+	    }
+	}
+
+	public Bitmap getBitmapFromMemCache(String key) {
+	    return mMemoryCache.get(key);
+	}
+	
+	public void loadBitmap(int resId, ImageView imageView) {
+	    final String imageKey = String.valueOf(resId);
+
+	    final Bitmap bitmap = getBitmapFromMemCache(imageKey);
+	    if (bitmap != null) {
+	    	imageView.setImageBitmap(bitmap);
+	    } else {
+		    if (cancelPotentialWork(resId, imageView)) {
+		        final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+		        final AsyncDrawable asyncDrawable =
+		                new AsyncDrawable(getResources(), mPlaceHolderBitmap, task);
+		        imageView.setImageDrawable(asyncDrawable);
+		        task.execute(resId);
+		    }
+	    }
+	}
+	
+	class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+	    private final WeakReference<ImageView> imageViewReference;
+	    private int data = 0;
+
+	    public BitmapWorkerTask(ImageView imageView) {
+	        imageViewReference = new WeakReference<ImageView>(imageView);
+	    }
+
+	    @Override
+	    protected Bitmap doInBackground(Integer... params) {
+	        data = params[0];
+	        final Bitmap bitmap =  BitmapFactory.decodeResource(getResources(), data);
+	        addBitmapToMemoryCache(String.valueOf(params[0]), bitmap);
+	        return bitmap;
+	    }
+
+	    @Override
+	    protected void onPostExecute(Bitmap bitmap) {
+	        if (isCancelled()) {
+	            bitmap = null;
+	        }
+
+	        if (imageViewReference != null && bitmap != null) {
+	            final ImageView imageView = imageViewReference.get();
+	            final BitmapWorkerTask bitmapWorkerTask =  getBitmapWorkerTask(imageView);
+	            if (this == bitmapWorkerTask && imageView != null) {
+	                imageView.setImageBitmap(bitmap);
+	            }
+	        }
+	    }
+	}
+
+	public static boolean cancelPotentialWork(int data, ImageView imageView) {
+	    final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+
+	    if (bitmapWorkerTask != null) {
+	        final int bitmapData = bitmapWorkerTask.data;
+	        if (bitmapData != data) {
+	            bitmapWorkerTask.cancel(true);
+	        } else {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
+	private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+		if (imageView != null) {
+			final Drawable drawable = imageView.getDrawable();
+			if (drawable instanceof AsyncDrawable) {
+				final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+				return asyncDrawable.getBitmapWorkerTask();
+			}
+		}
+		return null;
+	}
+	public static class AsyncDrawable extends BitmapDrawable {
+	    private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+
+	    public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
+	        super(res, bitmap);
+	        bitmapWorkerTaskReference =
+	            new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
+	    }
+
+	    public BitmapWorkerTask getBitmapWorkerTask() {
+	        return bitmapWorkerTaskReference.get();
+	    }
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+        // Pause fetcher to ensure smoother scrolling when flinging
+        if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+            // Before Honeycomb pause image loading on scroll to help with performance
+            if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)) {
+                setPauseWork(true);
+            }
+        } else {
+        	setPauseWork(false);
+        }
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		
+	}
+	
+    public void setPauseWork(boolean pauseWork) {
+        synchronized (mPauseWorkLock) {
+            mPauseWork = pauseWork;
+            if (!mPauseWork) {
+                mPauseWorkLock.notifyAll();
+            }
+        }
+    }
+}
--- sample/src/dev/dworks/libs/astickyheader/demo/ListActivity.java
+++ sample/src/dev/dworks/libs/astickyheader/demo/ListActivity.java
@@ -2,7 +2,6 @@ package dev.dworks.libs.astickyheader.demo;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.SparseArray;
@@ -11,13 +10,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import dev.dworks.libs.astickyheader.R;
 import dev.dworks.libs.astickyheader.SimpleSectionedListAdapter;
 import dev.dworks.libs.astickyheader.SimpleSectionedListAdapter.Section;
 
-public class ListActivity extends Activity {
+public class ListActivity extends CacheActivity {
 	private ListView list;
 	private ImageAdapter mAdapter;
 	private ArrayList<Section> sections = new ArrayList<Section>();
@@ -72,30 +72,35 @@ public class ListActivity extends Activity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			ImageView image;
-			
+			TextView name;
 			if (convertView == null) {
 				convertView = mInflater.inflate(R.layout.list_item, parent, false);
 			}
 
 			image = ViewHolder.get(convertView, R.id.image);
-			image.setImageResource(mImageIds[position]);
+			loadBitmap(mImageIds[position], image);
+			name = ViewHolder.get(convertView, R.id.name);
+			name.setText("A Cute Cat"+(position + 1));
 			return convertView;
 		}
 
 	}
 	
-    private Integer[] mImageIds = { R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
-    		R.drawable.empty_photo,R.drawable.empty_photo,
+    private Integer[] mImageIds = { 
+    		R.drawable.cat1,R.drawable.cat2,R.drawable.cat3,R.drawable.cat4,R.drawable.cat5,R.drawable.cat6,R.drawable.cat7,R.drawable.cat8,R.drawable.cat9,R.drawable.cat10,
+    		R.drawable.cat11,R.drawable.cat12,R.drawable.cat13,R.drawable.cat14,R.drawable.cat15,R.drawable.cat16,R.drawable.cat17,R.drawable.cat18,R.drawable.cat19,R.drawable.cat20,
+    		R.drawable.cat21,R.drawable.cat22,R.drawable.cat23,R.drawable.cat24,R.drawable.cat25,R.drawable.cat26,R.drawable.cat27,R.drawable.cat28,R.drawable.cat29,R.drawable.cat30,
+    		R.drawable.cat31,R.drawable.cat32,R.drawable.cat33,R.drawable.cat34,R.drawable.cat35,R.drawable.cat36,R.drawable.cat37,R.drawable.cat38,R.drawable.cat39,R.drawable.cat40,
+    		R.drawable.cat41,R.drawable.cat42,R.drawable.cat43,R.drawable.cat44,R.drawable.cat45,R.drawable.cat46,R.drawable.cat47,R.drawable.cat48,R.drawable.cat49,R.drawable.cat50,
+    		R.drawable.cat51,R.drawable.cat52,R.drawable.cat53,R.drawable.cat54,R.drawable.cat55,R.drawable.cat56,R.drawable.cat57,R.drawable.cat58,R.drawable.cat59,R.drawable.cat60,
+    		R.drawable.cat61,R.drawable.cat62,R.drawable.cat63,R.drawable.cat64,R.drawable.cat65,R.drawable.cat66,R.drawable.cat67,R.drawable.cat68,R.drawable.cat69,R.drawable.cat70,
+    		R.drawable.cat71,R.drawable.cat72,R.drawable.cat73,R.drawable.cat74,R.drawable.cat75,R.drawable.cat76,R.drawable.cat77,R.drawable.cat78,R.drawable.cat79,R.drawable.cat80,
+    		R.drawable.cat81,R.drawable.cat82,R.drawable.cat83,R.drawable.cat84,R.drawable.cat85,R.drawable.cat86,R.drawable.cat87,R.drawable.cat88,R.drawable.cat89,R.drawable.cat90,
+    		R.drawable.cat91,R.drawable.cat92,R.drawable.cat93,R.drawable.cat94,R.drawable.cat95,R.drawable.cat96,R.drawable.cat97,R.drawable.cat98,R.drawable.cat99,R.drawable.cat100,
     };
     
-    private String[] mHeaderNames = { "A Header", "One Header", "Some Header" };
-    private Integer[] mHeaderPositions = { 0, 6, 11 };
-
+    private String[] mHeaderNames = { "Cute Cats", "Few Cats", "Some Cats", "Some More Cats", "Some More More Cats", "Many Cats", "Many Many Cats", "So Many Cats" };
+    private Integer[] mHeaderPositions = { 0, 6, 11, 37, 38, 60, 77, 89 };
 
 	public static class ViewHolder {
 		@SuppressWarnings("unchecked")
